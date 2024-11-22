53cf96b44ada2e98327d875c6a9c0917bd665832
sergey-frey <sergey.frey.work@gmail.com>
2024-11-22 11:49:59 +0300

    feat: add env mode support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+const isProd = process.env.MODE === "production";
+const base = isProd ? "/profile/" : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [svelte()],
-})
+});
